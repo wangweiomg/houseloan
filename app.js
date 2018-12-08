266d2ba87667dbd4464fa65634349f681aef5df4
weiw <weiw@opera.com>
2018-12-08 19:09:16 +0800

    some update for get userinfo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -12,42 +12,29 @@ App({
         const ctx = this;
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         console.log('wx.login-->', res);
-        let url = 'https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&js_code=';
+     
+
+        let url = 'https://honeywen.com/wx/user/' + ctx.globalData.appKey.appid + '/login?code=' + res.code;
         wx.request({
-          url: url + res.code,
+          url: url,
           method: 'GET',
           data: ctx.globalData.appKey,
-          success(result){
-            console.log('openid-->{}', result);
-
+          success(result) {
+            let sessionKey = result.data.sessionKey;
+            let openid = result.data.openid;
+            console.log('wx/user/result-->', result, sessionKey, openid);
+            ctx.globalData.openid = openid;
+            ctx.globalData.sessionKey = sessionKey;
           }
         });
       }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              console.log('wx.getUserInfo-->', res);
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    });
+    
   },
   globalData: {
     userInfo: null,
+    openid: null,
+    sessionKey: null,
     appKey: {
       appid: 'wxd8871037033bb213',
       secret: 'd4d6d680d855162b2b4763433c9ff3b5'
--- pages/index/index.js
+++ pages/index/index.js
@@ -20,47 +20,6 @@ Page({
     });
   },
 
-  sendMsg: ()=>{
-    console.log('send');
-    let url1 = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxd8871037033bb213&secret=d4d6d680d855162b2b4763433c9ff3b5";
-
-    let url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=";
-    let templateURl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token="
-    const ctx = this;
-    wx.request({
-      url: url1,
-      method: 'GET',
-      success(res) {
-        console.log(res);
-        let token = res.data.access_token;
-
-        let param = {
-          touser: 'oMRgU0VI8fJnvgE472EIVOTUmVuc',
-          msgtype: 'text',
-          "text": {
-            content: 'hello world'
-            }
-        
-        };
-        let param2 = {
-          touser: 'oMRgU0VI8fJnvgE472EIVOTUmVuc',
-          template_id: 'rZqHkCsuzIlmTcs-F_gEK06AEjjDxRb3XsH4lYsXu2o',
-          page: 'index',
-          form_id: '1541515433598'
-
-        };
-        wx.request({
-          url: templateURl + token,
-          method: 'POST',
-          data: param2,
-          success(result) {
-            console.log('send msg-->', result);
-          }
-        });
-      }
-    })
-
-  },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -96,11 +55,28 @@ Page({
     }
   },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
+    console.log('userinfo-->', e);
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+
+    let encryptedData = e.detail.encryptedData;
+    let iv = e.detail.iv;
+    let rawData = e.detail.rawData;
+    let signature = e.detail.signature;
+    let sessionKey = app.globalData.sessionKey;
+
+    wx.request({
+      url: 'https://honeywen.com/wx/user/' + app.globalData.appKey.appid + '/info',
+      method: 'GET',
+      data: {signature: signature, sessionKey: sessionKey, rawData: rawData, iv: iv, encryptedData: encryptedData},
+      success: res=>{
+        console.log('/wx/info-->', res);
+
+      }
+    })
+
   }
 })
