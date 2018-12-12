707d3638d188d520ccad72e6e05cf0e69df09644
weiw <weiw@opera.com>
2018-12-12 22:48:48 +0800

    add callback

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ App({
             console.log('wx/user/result-->', result, sessionKey, openid);
             ctx.globalData.openid = openid;
             ctx.globalData.sessionKey = sessionKey;
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            if (this.userInfoReadyCallback) {
+              this.userInfoReadyCallback(result);
+            }
           }
         });
       }
